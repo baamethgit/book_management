570fe6dc8608252e6f4ddd6020374266306d6091
amethba <baameth@ept.sn>
2025-06-04 14:21:18 +0000

    mvp

--- bookManagement/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (email, password) VALUES ('baameth@ept.sn', '$2y$10$.qkbukzzX21D.bqbI.B2R.tvWP90o/Y16QRWVLodw51BHft7ZWbc.'),
-                                                     ('ba_amina12@gmail.com', '$2y$10$kp1V7UYDEWn17WSK16UcmOnFd1mPFVF6UkLrOOCGtf24HOYt8p1iC');
-]
